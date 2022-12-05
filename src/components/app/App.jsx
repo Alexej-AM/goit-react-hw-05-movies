42a16574593a224879019c961fe4eaad442329a2
Alexej Melnyk <98652436+Alexej-AM@users.noreply.github.com>
2022-12-05 23:39:58 +0200

    test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,8 @@
 import { lazy } from 'react';
-
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'components/layout/Layout';
 import { Home } from 'components/home/Home';
 import { StyledApp } from './AppStyled';
-// import { Movies } from 'components/movie/Movies';
-// import { MovieDetails } from 'components/movieDetails/MovieDetails';
 import { Cast } from 'components/cast/Cast';
 import { Reviews }  from 'components/reviews/Reviews';
 
@@ -23,7 +20,6 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
         </Route>
       </Routes>
     </StyledApp>
